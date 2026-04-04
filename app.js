7ce6a0171ba73571f668f2ed34cb447ec2367421
Kevin Martín Price <k.pricevisuals@gmail.com>
2026-04-04 19:32:49 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domin = ['.com',' .net', '.io', '.es'];
+
+for (let i = 0; i < pronoun.length; i++) {
+
+ 
+ for (let j = 0; j < adj.length; j++) {
+
+  
+ for (let k = 0; k < noun.length; k++) {
+
+ for (let d = 0; d < domin.length; d++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + domin[d]);
+      }
+    }
+  }
+}
